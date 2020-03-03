85501490cbb684ed7aebd61c936c1c132e52b001
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-03 15:31:31 -0700

    Create main.py

---
+++ program9/main.py
@@ -0,0 +1,6 @@
+#http://www.cs.princeton.edu/courses/archive/spring10/cos226/assignments/8puzzle.html
+#http://cit.dixie.edu/cs/2420/programs/8puzzle.htm
+#https://blog.goodaudience.com/solving-8-puzzle-using-a-algorithm-7b509c331288
+#https://sandipanweb.wordpress.com/2017/03/16/using-uninformed-informed-search-algorithms-to-solve-8-puzzle-n-puzzle/
+#https://codereview.stackexchange.com/questions/166603/using-bfs-to-solve-8-puzzle-game-using-python-2
+#https://stackoverflow.com/questions/1878260/tackling-the-8-puzzle-via-bfs
